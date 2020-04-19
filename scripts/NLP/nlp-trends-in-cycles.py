e8b7d5d4c7f4cb7faf7bb23cab06d42b44a2d97d
AnnaAD <anna.arpacidusseau@gmail.com>
2020-04-18 20:32:00 -0500

    More plots

--- scripts/NLP/nlp-trends-in-cycles.py
+++ scripts/NLP/nlp-trends-in-cycles.py
@@ -2,28 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("../../data/debate_transcripts_v3_sentiment.csv")
-by_cycle_subjectivity = data.groupby("election_cycle")["subjectivity"].mean()
-by_cycle_polarity = data.groupby("election_cycle")["polarity"].mean()
-
+select_cycle = data[data["election_cycle"] == 2016]
+print(select_cycle)
 
 fig, ax1 = plt.subplots()
 
-plt.xticks(by_cycle_polarity.index, rotation = 90)
-
 color = 'tab:red'
-ax1.set_xlabel('Election Cycle')
+ax1.set_xlabel('Debates in the 2016 Election Cycle')
 ax1.set_ylabel('Polarity', color=color)
-ax1.plot(by_cycle_polarity, color=color)
+ax1.plot(select_cycle["polarity"], color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:blue'
 ax2.set_ylabel('Subjectivity', color=color)  # we already handled the x-label with ax1
-ax2.plot(by_cycle_subjectivity, color=color)
+ax2.plot(select_cycle["subjectivity"], color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 print(plt.xticks())
-plt.savefig('../../plots/election_cycle_means.png')
+plt.savefig('../../plots/2016-election-trends.png')
+
 plt.show()
--- scripts/NLP/single-party-plots.py
+++ scripts/NLP/single-party-plots.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("../../data/debate_transcripts_v3_sentiment.csv")
-by_cycle_subjectivity = data.groupby("election_cycle")["subjectivity"].mean()
-by_cycle_polarity = data.groupby("election_cycle")["polarity"].mean()
+by_cycle_subjectivity = data[data["party"]=="Democratic Party"].groupby("election_cycle")["subjectivity"].mean()
+by_cycle_polarity = data[data["party"]=="Democratic Party"].groupby("election_cycle")["polarity"].mean()
 
 
 fig, ax1 = plt.subplots()
@@ -25,5 +25,5 @@ ax2.tick_params(axis='y', labelcolor=color)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 print(plt.xticks())
-plt.savefig('../../plots/election_cycle_means.png')
+plt.savefig('../../plots/election_cycle_means_dem.png')
 plt.show()
